9e5efcaee86528929b7f50dc5052b3240beeede2
Rafael Rarvs <rafael.rarvs@gmail.com>
2024-03-05 11:07:45 -0300

    algumas

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Lexer lex = new Lexer();
+        Parser parse = new Parser();
+        System.out.write('\n');
+    }
+}
